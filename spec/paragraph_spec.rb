611c8592db438a5fabad17b1e66a4ebcb52b50c8
Brad Ediger <brad.ediger@madriska.com>
2010-02-21 21:02:15 -0600

    Refactor paragraph specs; specify total w/y/z values

--- spec/paragraph_spec.rb
+++ spec/paragraph_spec.rb
@@ -3,21 +3,31 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")
 describe "each_legal_breakpoint" do
   include GangstaWrap
 
-  it "should yield at legal breakpoints" do
-    stream = [
-      b1=box(100),
-      g2=glue(10, 5, 3),
-      b3=box(100),
-      p4=penalty(50),
-      g5=glue(10, 5, 3),
-      b6=box(100)
+  before(:each) do
+    @stream = [
+      @b1=box(100),
+      @g2=glue(10, 5, 3),
+      @b3=box(100),
+      @p4=penalty(50),
+      @g5=glue(10, 5, 3),
+      @b6=box(100)
     ]
 
-    para = Paragraph.new(stream)
-    breaks = []
-    para.each_legal_breakpoint { |item, *_| breaks << item }
+    @para = Paragraph.new(@stream)
+  end
 
-    breaks.should == [g2, p4]
+  it "should yield at legal breakpoints, with correct width/stretch/shrink" do
+    breaks = []
+    totals = []
+    @para.each_legal_breakpoint do |item, i, tw, ty, tz| 
+      breaks << item
+      totals << [tw, ty, tz]
+    end
+    # These can be broken at the first glue or the first penalty. (Cannot break
+    # at g5 because it is not preceded by a box.)
+    breaks.should == [@g2, @p4]
+    totals.should == [[100, 0, 0], # before g2
+                      [210, 5, 3]] # before p4
   end
 end
 
