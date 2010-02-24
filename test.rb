ee49bae284f82866562117ee16db011e6e5cb545
Brad Ediger <brad.ediger@madriska.com>
2010-02-23 20:06:36 -0600

    Updated example to patch around tokenizer problem with hyphens and newlines

--- test.rb
+++ test.rb
@@ -11,14 +11,14 @@ on this continent a new nation, conceived in liberty, and
 dedicated to the proposition that all men are created equal.
 Now we are engaged in a great civil war, testing
 whether that nation, or any nation so conceived and so
-dedicated, can long endure. We are met on a great battle-
-field of that war. We have come to dedicate a portion of
+dedicated, can long endure. We are met on a great battle-field
+of that war. We have come to dedicate a portion of
 that field as a final resting-place for those who here gave
 their lives that that nation might live. It is altogether
 fitting and proper that we should do this.
 END
 
-para = GangstaWrap::Paragraph.new(stream, :width => 400)
+para = GangstaWrap::Paragraph.new(stream, :width => 200)
 
 puts "Stream length: #{stream.length}"
 puts "Active positions: #{para.optimum_breakpoints.map{|x| x.position}}"
